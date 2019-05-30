45cec219b68cb29143753ea033f6171f67af19e3
JS Pro <jspro@ES-MBP-TX1159-AM.local>
2019-05-30 02:14:10 -0500

    added to do list and custom hooks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react';
-//import './App.css';
-import './components/AddToDo';
-import Layout from './components/Layout';
-import AddToDo from './components/AddToDo';
+//import "./styles.css";
 
-function App() {
+import React, { memo } from "react";
+//import ReactDOM from "react-dom";
+
+import { useInputValue, useTodos } from "./custom-hooks";
+
+import Layout from "./components/Layout";
+
+import AddTodo from "./components/AddToDo";
+import TodoList from "./components/ToDoList"
+
+const TodoApp = memo(props => {
+  const { inputValue, changeInput, clearInput, keyInput } = useInputValue();
+  const { todos, addTodo, checkTodo, removeTodo } = useTodos();
+
+  const clearInputAndAddTodo = _ => {
+    clearInput();
+    addTodo(inputValue);
+  };
+
   return (
-    <div>
-      <header>
-      </header>
-      <main>
-        <Layout />
-        <AddToDo />
-      </main>
-    </div>
+    <Layout>
+      <AddTodo
+        inputValue={inputValue}
+        onInputChange={changeInput}
+        onButtonClick={clearInputAndAddTodo}
+        onInputKeyPress={event => keyInput(event, clearInputAndAddTodo)}
+      />
+      <TodoList
+        items={todos}
+        onItemCheck={idx => checkTodo(idx)}
+        onItemRemove={idx => removeTodo(idx)}
+      />
+    </Layout>
   );
-}
+});
 
-export default App;
+export default TodoApp;
